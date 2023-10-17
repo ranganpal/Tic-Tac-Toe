97c1a86f63069c0d6fa1bd6685a2d14b9f74aada
ranganpal <ranganpal5@gmail.com>
2023-10-17 17:47:36 +0530

    improvement in game logic click event

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 console.log("Welcome to Tic Tac Toe");
 
-let turn = "X"
-const turnMusic = new Audio("assets/ting.mp3")
-const winMusic = new Audio("assets/gameover.mp3")
-let isGameOver = false
-let isDraw = false
+let turn = "X";
+const turnMusic = new Audio("assets/ting.mp3");
+const winMusic = new Audio("assets/gameover.mp3");
+let isGameOver = false;
+let isDraw = false;
 
 // Function to change the turn
 const changeTurn = () => {
-    return turn === "X" ? "O" : "X"
-}
+    turn = turn === "X" ? "O" : "X";
+    turnMusic.play();
+};
 
 // Function to chech win
 const checkWin = () => {
@@ -22,98 +23,92 @@ const checkWin = () => {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ]
+    ];
 
     win.forEach((e) => {
-        let boxTexts = document.getElementsByClassName("boxText")
+        let boxTexts = document.getElementsByClassName("boxText");
         if (boxTexts[e[0]].innerText === boxTexts[e[1]].innerText &&
             boxTexts[e[1]].innerText === boxTexts[e[2]].innerText &&
             boxTexts[e[0]].innerText !== "") {
-            document.querySelector(".info").innerText = boxTexts[e[0]].innerText + " has won"
-            isGameOver = true
-            document.getElementsByTagName("img")[0].style.width = "250px"
-            winMusic.play()
+            document.querySelector(".info").innerText = boxTexts[e[0]].innerText + " has won";
+            document.getElementsByTagName("img")[0].style.width = "250px";
+            winMusic.play();
+            isGameOver = true;
 
-            let boxes = document.getElementsByClassName("box")
+            let boxes = document.getElementsByClassName("box");
             e.forEach((i) => {
-                boxes[i].style.background = "#f2c14e25"
-                boxes[i].classList.add("changeBG")
-            })
+                boxes[i].style.background = "#f2c14e25";
+                boxes[i].classList.add("changeBG");
+            });
         }
-    })
+    });
 }
 
 // Function to chech draw
 const checkDraw = () => {
-    let boxTexts = document.getElementsByClassName("boxText")
-    let isFull = true
+    let boxTexts = document.getElementsByClassName("boxText");
+    let isFull = true;
     Array.from(boxTexts).forEach((element) => {
         if (element.innerText == "") {
-            isFull = false
+            isFull = false;
         }
-    })
+    });
     if (isFull && !isGameOver) {
-        isDraw = true
-        document.querySelector(".info").innerText = "Match Draw ! ! !"
-        document.getElementsByTagName("img")[1].style.width = "250px"
+        document.querySelector(".info").innerText = "Match Draw ! ! !";
+        document.getElementsByTagName("img")[1].style.width = "250px";
+        isDraw = true;
     }
-}
+};
 
 
 // Game Logic
-let boxes = document.getElementsByClassName("box")
+let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach((element) => {
-    let boxText = element.querySelector(".boxText")
+    let boxText = element.querySelector(".boxText");
     element.addEventListener('click', () => {
-        if (boxText.innerText === "") {
-            if (!isGameOver) {
-                boxText.innerText = turn
-                turn = changeTurn()
-                turnMusic.play()
-                checkWin()
-                checkDraw()
-                if (!isGameOver && !isDraw) {
-                    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn
-                }
-            }
-            else {
-                boxText.innerText = ""
+        if (boxText.innerText === "" && !isGameOver) {
+            boxText.innerText = turn;
+            changeTurn();
+            checkWin();
+            checkDraw();
+            if (!isGameOver && !isDraw) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
         }
-    })
-    element.addEventListener('mouseover', () =>{
-        if(!element.className.includes("changeBG")){
-            element.style.background = "#273841"
+    });
+    element.addEventListener('mouseover', () => {
+        if (!element.className.includes("changeBG")) {
+            element.style.background = "#273841";
         }
-    })
-    element.addEventListener('mouseout', () =>{
-        if(!element.className.includes("changeBG")){
-            element.style.background = "#2d414b"
+    });
+    element.addEventListener('mouseout', () => {
+        if (!element.className.includes("changeBG")) {
+            element.style.background = "#2d414b";
         }
-    })
-})
+    });
+});
 
 // Add onclick listener to restart button
-const restart = document.getElementById("restart")
+const restart = document.getElementById("restart");
 restart.addEventListener('click', () => {
-    let boxTexts = document.getElementsByClassName("boxText")
+    let boxTexts = document.getElementsByClassName("boxText");
     Array.from(boxTexts).forEach((element) => {
-        element.innerText = ""
-    })
-    turn = "X"
-    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn
+        element.innerText = "";
+    });
+    turn = "X";
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
 
-    let img = document.getElementsByTagName("img")
+    let img = document.getElementsByTagName("img");
     Array.from(img).forEach((element) => {
-        element.style.width = "0"
-    })
+        element.style.width = "0";
+    });
 
-    let changeBG = document.getElementsByClassName("changeBG")
+    let changeBG = document.getElementsByClassName("changeBG");
     Array.from(changeBG).forEach((element) => {
-        element.style.backgroundColor = "#2d414b"
-        element.classList.remove("changeBG")
-    })
+        element.style.backgroundColor = "#2d414b";
+        element.classList.remove("changeBG");
+    });
 
-    isGameOver = false
-    isDraw = false
-})
+    isGameOver = false;
+    isDraw = false;
+});
